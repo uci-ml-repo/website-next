e0d929ae07d3ce1fba8f26b1a623c76ac335a531
Andrew Wang <awwang3@uci.edu>
2024-12-18 18:46:54 -0800

    feat: update copy shell script

--- legacy/files/copy-remote.sh
+++ legacy/files/copy-remote.sh
@@ -2,13 +2,15 @@
 
 source ../.env
 
-if [[ -z "$HOST" || -z "$STATIC_FILES_PATH" ]]; then
+if [[ -z "$HOST" || -z "$STATIC_FILES_PATH" || -z "$COPY_TO_PATH" ]]; then
+  echo "Required variables are not set: HOST, STATIC_FILES_PATH, COPY_TO_PATH"
   exit 1
 fi
 
-rm -rf ./static/*
+rm -rf "${COPY_TO_PATH:?}/*"
 
-echo "$HOST:$STATIC_FILES_PATH"
+echo "$HOST:$STATIC_FILES_PATH => $COPY_TO_PATH"
 
+# Find and exclude files larger than 512MB
 ssh "$HOST" "find \"$STATIC_FILES_PATH\" -type f -size +512M" > ignored
-rsync -az --info=progress2 --exclude-from=ignored "$HOST:$STATIC_FILES_PATH/" ./static
+rsync -az --info=progress2 --exclude-from=ignored "$HOST:$STATIC_FILES_PATH/" "$COPY_TO_PATH/"
