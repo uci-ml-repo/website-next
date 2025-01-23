6110e602703f0027a684244e2720f4a505ecda3c
Andrew Wang <awwang3@uci.edu>
2025-01-23 10:11:38 -0800

    fix: stuff

--- src/components/dataset/page/tabs/files/Files.tsx
+++ src/components/dataset/page/tabs/files/Files.tsx
@@ -4,11 +4,15 @@ import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import { useEffect, useState } from "react";
 
 import FilesBrowse from "@/components/dataset/page/tabs/files/browse/FilesBrowse";
-import { useFileContext } from "@/components/dataset/page/tabs/files/FilesContext";
+import {
+  FileProvider,
+  useFileContext,
+} from "@/components/dataset/page/tabs/files/FilesContext";
 import FilesView from "@/components/dataset/page/tabs/files/view/FilesView";
 import { useIsMobile } from "@/components/hooks/use-mobile";
 import Split from "@/components/ui/split";
 import type { DatasetResponse } from "@/lib/types";
+import { datasetFilesPath } from "@/lib/utils";
 
 export default function Files({ dataset }: { dataset: DatasetResponse }) {
   const [splitSizes, setSplitSizes] = useState<[number, number]>([20, 80]);
@@ -31,38 +35,42 @@ export default function Files({ dataset }: { dataset: DatasetResponse }) {
   }, [currentFile, isMobile]);
 
   return (
-    <div className="flex h-full flex-col space-y-2">
-      <div className="flex justify-between md:hidden">
-        <button
-          onClick={() => setSplitSizes([100, 0])}
-          className="flex disabled:invisible"
-          disabled={splitSizes[1] < 50}
+    <FileProvider
+      initialPath={{ path: datasetFilesPath(dataset), type: "directory" }}
+    >
+      <div className="flex h-full flex-col space-y-2">
+        <div className="flex justify-between md:hidden">
+          <button
+            onClick={() => setSplitSizes([100, 0])}
+            className="flex disabled:invisible"
+            disabled={splitSizes[1] < 50}
+          >
+            <ChevronLeftIcon />
+            <span>Browse</span>
+          </button>
+          <button
+            onClick={() => setSplitSizes([0, 100])}
+            disabled={splitSizes[0] < 50}
+            className="flex disabled:invisible"
+          >
+            <span>View File</span>
+            <ChevronRightIcon />
+          </button>
+        </div>
+        <Split
+          className="h-[75dvh]"
+          gutterSize={12}
+          sizes={splitSizes}
+          setSizes={setSplitSizes}
         >
-          <ChevronLeftIcon />
-          <span>Browse</span>
-        </button>
-        <button
-          onClick={() => setSplitSizes([0, 100])}
-          disabled={splitSizes[0] < 50}
-          className="flex disabled:invisible"
-        >
-          <span>View File</span>
-          <ChevronRightIcon />
-        </button>
+          <div className="flex h-full flex-col overflow-hidden rounded-l-xl border-2 border-r-0">
+            <FilesBrowse dataset={dataset} />
+          </div>
+          <div className="flex h-full flex-col overflow-hidden rounded-r-xl border-2 border-l-0">
+            <FilesView dataset={dataset} />
+          </div>
+        </Split>
       </div>
-      <Split
-        className="h-[75dvh]"
-        gutterSize={12}
-        sizes={splitSizes}
-        setSizes={setSplitSizes}
-      >
-        <div className="flex h-full flex-col overflow-hidden rounded-l-xl border-2 border-r-0">
-          <FilesBrowse dataset={dataset} />
-        </div>
-        <div className="flex h-full flex-col overflow-hidden rounded-r-xl border-2 border-l-0">
-          <FilesView dataset={dataset} />
-        </div>
-      </Split>
-    </div>
+    </FileProvider>
   );
 }
