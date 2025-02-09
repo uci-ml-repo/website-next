126b7c6c0cb3cf9ca34779d1c5bf211785a5c40e
Andrew Wang <awwang3@uci.edu>
2025-02-08 18:13:42 -0800

    fix: stuff

--- src/components/auth/VerificationError.tsx
+++ src/components/auth/VerificationError.tsx
@@ -1,6 +1,15 @@
 import { ErrorGraphic } from "@/components/layout/ErrorGraphic";
+import { PROFILE_SETTINGS_ROUTE } from "@/lib/routes";
 
 export function VerificationError() {
-  // TODO
-  return <ErrorGraphic>X</ErrorGraphic>;
+  return (
+    <ErrorGraphic header="Verification Required">
+      <div className="text-lg">
+        <span>To continue, </span>
+        <a href={PROFILE_SETTINGS_ROUTE} className="text-link hover:underline">
+          verify your email address
+        </a>
+      </div>
+    </ErrorGraphic>
+  );
 }
