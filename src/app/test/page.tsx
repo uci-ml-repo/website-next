cada630a1ec748cb576fea619b3ff6515b2108b2
Andrew Wang <awwang3@uci.edu>
2025-02-13 13:28:32 -0800

    fix: stuff

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,14 +1,5 @@
-"use client";
-
-import { getMaterializedViewConfig } from "drizzle-orm/pg-core";
-
 import { Main } from "@/components/layout/Main";
-import { datasetView } from "@/db/schema";
 
 export default function Test() {
-  const cols = getMaterializedViewConfig(datasetView).selectedFields;
-
-  console.log(JSON.stringify(cols));
-
   return <Main />;
 }
