abc79cb23202ee52272eb1a3ffc2978835649a5e
Andrew Wang <awwang3@uci.edu>
2025-07-20 16:56:12 -0400

    chore: init sst

--- apps/website/src/app/auth/login/page.tsx
+++ apps/website/src/app/auth/login/page.tsx
@@ -1,4 +1,10 @@
 import { Button } from "@website/components/ui/button";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login",
+  description: "Login or Sign Up for a UCI Machine Learning Repository account.",
+};
 
 export default function Page() {
   return (
--- apps/website/src/app/datasets/page.tsx
+++ apps/website/src/app/datasets/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Datasets",
+  description: "Browse datasets from the UCI Machine Learning Repository.",
+};
+
 export default function Page() {
   return <div>DATASETS</div>;
 }
