40ff7d365280a325150646d14776eb8c1786b635
Andrew Wang <awwang3@uci.edu>
2025-07-22 21:13:53 -0400

    chore: update deps

--- apps/website/sst-env.d.ts
+++ apps/website/sst-env.d.ts
@@ -5,13 +5,13 @@
 
 declare module "sst" {
   export interface Resource {
-    website: {
-      type: "sst.aws.Nextjs";
-      url: string;
-    };
+    "website": {
+      "type": "sst.aws.Nextjs"
+      "url": string
+    }
   }
 }
 /// <reference path="sst-env.d.ts" />
 
-import "sst";
-export {};
+import "sst"
+export {}
