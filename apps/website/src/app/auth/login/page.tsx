2a8f49a231bd83947016467b2a103a99d990a57e
Andrew Wang <awwang3@uci.edu>
2025-07-22 22:21:59 -0400

    chore: add db utils

--- apps/website/src/app/auth/login/page.tsx
+++ apps/website/src/app/auth/login/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@website/components/ui/button";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Login",
-  description: "Login or Sign Up for a UCI Machine Learning Repository account.",
+  title: "Login or Register",
+  description: "Login or register for a UCI Machine Learning Repository account.",
 };
 
 export default function Page() {
--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -5,8 +5,8 @@ import { defineConfig } from "drizzle-kit";
 import { env } from "./env";
 
 export default defineConfig({
-  out: "./src/db/migrations",
-  schema: "./src/db/schema.ts",
+  out: "./src/migrations",
+  schema: "./src/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: env.DATABASE_URL,
